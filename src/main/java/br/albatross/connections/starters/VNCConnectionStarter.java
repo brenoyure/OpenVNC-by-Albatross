1fbaa634f74d75f0e1f9b90d91354fd90f0e4a2f
Breno Yuri <byurifb@gmail.com>
2023-04-05 14:53:01 -0300

    First Release
    Dando uns retoques no App, como agrupando os botões radio, e dando a opção de ao pressionar ENTER, iniciar o acesso.

--- src/main/java/br/albatross/connections/starters/VNCConnectionStarter.java
+++ src/main/java/br/albatross/connections/starters/VNCConnectionStarter.java
@@ -15,10 +15,21 @@ public class VNCConnectionStarter implements ConnectionStarter {
         this.builder = builder;
     }
 
+    /**
+     * Starts a VNC Connection and put it's process/thread to sleep, using the waitFor() while
+     * connection is running, preventing opening multiple connections from the
+     * same instance.
+     *
+     * @param vncConnection
+     * @throws IOException if VNC_HOME envoriment variable does not exists, or
+     * it does not have the value of correct VNC installation folder
+     *
+     * @throws InterruptedException
+     */
     @Override
     public void startConnection(Connection vncConnection) throws IOException, InterruptedException {
         String connectionString = builder.getConnectionString(vncConnection);
-        getRuntime().exec(connectionString);
+        getRuntime().exec(connectionString).waitFor();
     }
 
 }
--- src/main/java/br/albatross/open/vnc/MainController.java
+++ src/main/java/br/albatross/open/vnc/MainController.java
@@ -10,25 +10,53 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.TextField;
 import javafx.scene.control.ToggleGroup;
-import javafx.scene.input.MouseDragEvent;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
 
 public class MainController {
 
+    /**
+     * Represents the text field in the GUI, where users type the IP or
+     * Hostname.
+     */
     @FXML
     private TextField host;
 
-    private final String userName   = System.getProperty("user.name");
-    private final String password   = System.getenv("domain_password");
-    private final String vncHomeDir = System.getenv("VNC_HOME");
+    /**
+     * Represents the Host-Radio-Button group in the GUI. This group of radio
+     * buttons is used so that users can select only ONE button.
+     */
     @FXML
     private ToggleGroup host_radio_button;
 
-    private void switchToSecondary() throws IOException {
-        App.setRoot("main");
+    /**
+     * Represents connection user name, that is recovered by the "user.name"
+     * System Property. Basically, the current logged user.
+     */
+    private final String userName = System.getProperty("user.name");
+    private final String password = System.getenv("domain_password");
+
+    /**
+     * Represents the Envoiroment Variable VNC_HOME, where the software
+     * ultraVNC® is installed.
+     */
+    private final String vncHomeDir = System.getenv("VNC_HOME");
+
+    /**
+     * Triggers the startConnection() when "Connect" button is clicked.
+     *
+     * @param btnClicked
+     */
+    @FXML
+    private void connectBtnClicked(ActionEvent btnClicked) {
+        startConnection();
     }
 
-    @FXML
-    private void connectBtnClicked(ActionEvent event) {
+    /**
+     * Triggers the Builder and Starter components to build and start a VNC
+     * Connection (on Windows).
+     */
+    private void startConnection() {
 
         ConnectionBuilder builder = new VncConnectionBuilder(vncHomeDir);
         Connection connection = builder.createConnection(host.getText(), userName, password);
@@ -43,9 +71,32 @@ public class MainController {
 
     }
 
+    /**
+     * Triggers the startConnection() when ENTER key is pressed.
+     *
+     * @param keyPressed
+     */
+//    private void hostConnectWhenEnterKeyPressed(KeyEvent keyPressed) {
+//        if (keyPressed.getCode().equals(KeyCode.ENTER)) {
+//            startConnection();
+//        }
+//
+//    }
+    /**
+     * Re enables the Connect button, in the GUI, if the host text field is not
+     * empty.
+     *
+     * @param keyType
+     */
+//    private void enableConnectBtn(KeyEvent keyType) {
+//        if (host.getText().length() > 0) {
+//            host.setDisable(false);
+//        }
+//    }
     @FXML
     private void andarTerroRadioBtnClicked(ActionEvent event) {
         host.setText("10.40.10.");
+
     }
 
     @FXML
